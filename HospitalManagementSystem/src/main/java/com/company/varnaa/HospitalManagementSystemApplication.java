7b390a957c93366a5e54cb33b804790aed084bd5
praveendev008 <pravindevkate08@gmail.com>
2022-06-10 14:32:24 +0530

    JAVA
    JAVA

---
+++ HospitalManagementSystem/src/main/java/com/company/varnaa/HospitalManagementSystemApplication.java
@@ -0,0 +1,20 @@
+package com.company.varnaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication
+@ComponentScan
+@EntityScan(basePackages = {"com.company.varnaa"})
+public class HospitalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementSystemApplication.class, args);
+	}
+
+}
